bdb267c602521cafeb26c30f90b7983a56cfccd7
mk1808 <37344587+mk1808@users.noreply.github.com>
2025-10-21 19:11:50 +0200

    #2: add gitignore and configure frontend build

--- userInterface/build.gradle.kts
+++ userInterface/build.gradle.kts
@@ -1,7 +1,11 @@
+import java.io.File
+import com.github.gradle.node.npm.task.NpmTask
+
 plugins {
 	java
 	id("org.springframework.boot") version "3.5.6"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("com.github.node-gradle.node") version "5.0.0"
 }
 
 group = "com.notifications"
@@ -27,3 +31,37 @@ dependencies {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+node {
+    download.set(false)
+    workDir.set(file("${project.projectDir}/src/main/ts"))
+    npmWorkDir.set(file("${project.projectDir}/src/main/ts"))
+    nodeProjectDir.set(file("${project.projectDir}/src/main/ts"))
+}
+
+tasks.register<NpmTask>("appNpmInstall") {
+    description = "read package.json and install all dependencies"
+    workingDir.set(file("${project.projectDir}/src/main/ts"))
+    args.set(listOf("install", "--loglevel=info")) 
+}
+
+tasks.register<NpmTask>("appNpmBuild") {
+    description = "Builds application for your frontend"
+    workingDir.set(file("${project.projectDir}/src/main/ts"))
+    args.set(listOf("run", "build"))
+}
+
+tasks.register<Copy>("copyToFrontend") {
+    description = "Copies frontend build files"
+    from("$projectDir/src/main/ts/dist/")
+    into("$projectDir/src/main/resources/static")
+}
+tasks.named("appNpmBuild") {
+    dependsOn("appNpmInstall")
+    finalizedBy("copyToFrontend")
+}
+
+tasks.named("compileJava") {
+    dependsOn("copyToFrontend")
+}
+
