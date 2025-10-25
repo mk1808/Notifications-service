a84d25391860c1d97efe6fd52baa1897e383157e
Marek Czopor <marqos111222@gmail.com>
2025-10-25 16:20:48 +0200

    mergefix

--- userInterface/build.gradle.kts
+++ userInterface/build.gradle.kts
@@ -27,7 +27,7 @@ extra["springCloudVersion"] = "2025.0.0"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
-  implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
