61589157ce413e29c2abb1216540e369dad53e5b
mk1808 <37344587+mk1808@users.noreply.github.com>
2025-10-26 21:58:47 +0100

    #5 create Spring project structure - notifications

--- notifications/src/main/java/com/notifications/workers/RabbitMQProducer.java
+++ notifications/src/main/java/com/notifications/workers/RabbitMQProducer.java
@@ -1,8 +1,10 @@
-package com.notifications.queue;
+package com.notifications.workers;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.notifications.configs.RabbitMQConfig;
 
 @Service
 public class RabbitMQProducer {
