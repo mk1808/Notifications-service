647007e2f8ad25bdc474d5c0d614fdfdaa640515
Marek Czopor <marqos111222@gmail.com>
2025-10-30 21:10:47 +0100

    22 SonarQube complains fix

--- userInterface/src/main/java/com/notifications/user_interface/UserInterfaceApplication.java
+++ userInterface/src/main/java/com/notifications/user_interface/UserInterfaceApplication.java
@@ -1,4 +1,4 @@
-package com.notifications.userInterface;
+package com.notifications.user_interface;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
