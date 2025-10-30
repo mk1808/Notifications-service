aa3a6ace99994abef507b0a7ec7a4275c9a59d91
Marek Czopor <marqos111222@gmail.com>
2025-10-30 18:06:16 +0100

    22 Configure eslint

--- userInterface/src/main/ts/eslint.config.js
+++ userInterface/src/main/ts/eslint.config.js
@@ -4,20 +4,161 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import checkFile from 'eslint-plugin-check-file';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import importPlugin from 'eslint-plugin-import';
+import eslintReact from "@eslint-react/eslint-plugin";
+import react from 'eslint-plugin-react';
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      'check-file': checkFile,
+      'react': react,
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      eslintPluginPrettierRecommended,
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+      eslintReact.configs["recommended-typescript"],
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
+    rules: {
+      'linebreak-style': ['error', 'windows'],
+      "indent": ["error", "tab"],
+      '@typescript-eslint/no-explicit-any': ['warn'],
+      '@typescript-eslint/explicit-function-return-type': ['off'],
+      '@typescript-eslint/no-unused-vars': ['error'],
+      'react/prop-types': 'off',
+      "react/prefer-stateless-function": "error",
+      "react/button-has-type": "error",
+      "react/no-unused-prop-types": "error",
+      "react/jsx-pascal-case": "error",
+      "react/jsx-no-script-url": "error",
+      "react/no-children-prop": "error",
+      "react/no-danger": "error",
+      "react/no-danger-with-children": "error",
+      "react/no-unstable-nested-components": ["error", { allowAsProps: true }],
+      "react/jsx-fragments": "error",
+      "react/destructuring-assignment": [
+        "error",
+        "always",
+        { destructureInSignature: "always" },
+      ],
+      "react/jsx-no-leaked-render": ["error", { validStrategies: ["ternary"] }],
+      "react/jsx-max-depth": ["error", { max: 5 }],
+      "react/function-component-definition": [
+        "warn",
+        { namedComponents: "arrow-function" },
+      ],
+      "react/jsx-key": [
+        "error",
+        {
+          checkFragmentShorthand: true,
+          checkKeyMustBeforeSpread: true,
+          warnOnDuplicates: true,
+        },
+      ],
+      "react/jsx-no-useless-fragment": "warn",
+      "react/jsx-curly-brace-presence": "warn",
+      "react/no-typos": "warn",
+      "react/display-name": "warn",
+      "react/self-closing-comp": "warn",
+      "react/jsx-sort-props": "warn",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-one-expression-per-line": "off",
+      "react/prop-types": "off",
+      'import/default': 'off',
+      'import/no-cycle': 'error',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+          ],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      'import/no-restricted-paths': [
+        'error',
+        {
+          zones: [
+            // disables cross-feature imports:
+            // eg. src/features/discussions should not import from src/features/comments, etc.
+            {
+              target: './src/features/auth',
+              from: './src/features',
+              except: ['./auth'],
+            },
+
+            // enforce unidirectional codebase:
+            // e.g. src/app can import from src/features but not the other way around
+            {
+              target: './src/features',
+              from: './src/app',
+            },
+
+            // e.g src/features and src/app can import from these shared modules but not the other way around
+            {
+              target: [
+                './src/components',
+                './src/hooks',
+                './src/lib',
+                './src/types',
+                './src/utils',
+              ],
+              from: ['./src/features', './src/app'],
+            },
+          ],
+        },
+      ],
+      'check-file/filename-naming-convention': [
+        'error',
+        {
+          '**/*.{jsx,tsx}': 'PASCAL_CASE',
+          '**/*.{js,ts}': 'CAMEL_CASE',
+        },
+      ],
+    },
+    "settings": {
+      "react": {
+        "version": "detect", 
+      },
+    }
   },
+  {
+    files: ['src/**/*.*'],
+    plugins: {
+      'check-file': checkFile,
+    },
+    rules: {
+      'check-file/folder-naming-convention': [
+        'error',
+        {
+          'src/*/': 'CAMEL_CASE',
+        },
+      ],
+    }
+  }
 ])
