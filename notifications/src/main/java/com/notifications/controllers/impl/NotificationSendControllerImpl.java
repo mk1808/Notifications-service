f015348e11c8c4697803e6bc826092feb21be72c
mk1808 <37344587+mk1808@users.noreply.github.com>
2025-11-05 22:23:55 +0100

    #28 add notification send controller

--- notifications/src/main/java/com/notifications/controllers/impl/NotificationSendControllerImpl.java
+++ notifications/src/main/java/com/notifications/controllers/impl/NotificationSendControllerImpl.java
@@ -1,7 +1,16 @@
 package com.notifications.controllers.impl;
 
+import org.springframework.http.ResponseEntity;
+
 import com.notifications.controllers.NotificationSendController;
+import com.notifications.dtos.NotificationDto;
 
 public class NotificationSendControllerImpl implements NotificationSendController {
+
+	@Override
+	public ResponseEntity<String> send(NotificationDto notificationDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
