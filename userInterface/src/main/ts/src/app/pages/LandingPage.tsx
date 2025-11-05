17178ef90db72913cd9d2f9440ea57a20dfc75fc
Marek Czopor <marqos111222@gmail.com>
2025-11-05 21:02:12 +0100

    #25 Config react router

--- userInterface/src/main/ts/src/app/pages/LandingPage.tsx
+++ userInterface/src/main/ts/src/app/pages/LandingPage.tsx
@@ -1,4 +1,6 @@
+import { HStack, StackSeparator } from "@chakra-ui/react";
 import { type JSX } from "react";
+import { Link } from "react-router-dom";
 
 interface LandingPageProps {
 	placeholder?: string;
@@ -8,6 +10,26 @@ const LandingPage = ({ placeholder }: LandingPageProps): JSX.Element => (
 	<div>
 		<h1>LandingPage component</h1>
 		{placeholder}
+		<HStack ml="10" mt="6" separator={<StackSeparator />}>
+			<Link to="/" viewTransition>
+				Home
+			</Link>
+			<Link to="/about" viewTransition>
+				About
+			</Link>
+			<Link to="/messages" viewTransition>
+				Messages
+			</Link>
+			<Link to="/test" viewTransition>
+				Test
+			</Link>
+			<Link to="/login" viewTransition>
+				Login
+			</Link>
+			<Link to="/not_existing" viewTransition>
+				Not existing
+			</Link>
+		</HStack>
 	</div>
 );
 
