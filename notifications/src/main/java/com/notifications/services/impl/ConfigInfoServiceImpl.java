f807473826b0c8b7ac4825e5cd4d7136a7324d08
mk1808 <37344587+mk1808@users.noreply.github.com>
2025-11-05 22:24:34 +0100

    #28 add services

---
+++ notifications/src/main/java/com/notifications/services/impl/ConfigInfoServiceImpl.java
@@ -0,0 +1,15 @@
+package com.notifications.services.impl;
+
+import com.notifications.dtos.NotificationDto;
+import com.notifications.services.ConfigInfoService;
+import com.notifications.services.NotificationSendService;
+
+public class ConfigInfoServiceImpl implements NotificationSendService, ConfigInfoService {
+
+	@Override
+	public void send(NotificationDto dto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ notifications/src/main/java/com/notifications/services/impl/RabbitMQServiceImpl.java
@@ -0,0 +1,15 @@
+package com.notifications.services.impl;
+
+import com.notifications.dtos.NotificationDto;
+import com.notifications.services.NotificationSendService;
+import com.notifications.services.RabbitMQService;
+
+public class RabbitMQServiceImpl implements NotificationSendService, RabbitMQService {
+
+	@Override
+	public void send(NotificationDto dto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
