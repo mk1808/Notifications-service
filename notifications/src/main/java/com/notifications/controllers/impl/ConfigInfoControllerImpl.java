33cf17537ac871a2e96827e3a03d5b27fcf50999
mk1808 <37344587+mk1808@users.noreply.github.com>
2025-11-05 22:20:26 +0100

    #28 add method in config

--- notifications/src/main/java/com/notifications/controllers/impl/ConfigInfoControllerImpl.java
+++ notifications/src/main/java/com/notifications/controllers/impl/ConfigInfoControllerImpl.java
@@ -1,7 +1,16 @@
 package com.notifications.controllers.impl;
 
+import org.springframework.http.ResponseEntity;
+
 import com.notifications.controllers.ConfigInfoController;
+import com.notifications.dtos.ConfigInfoDto;
 
 public class ConfigInfoControllerImpl implements ConfigInfoController {
+
+	@Override
+	public ResponseEntity<ConfigInfoDto> getCurrentConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
