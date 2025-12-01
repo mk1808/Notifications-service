6e5a0e1c8a55163533247bdc81c6003fb1bdf762
Marek Czopor <marqos111222@gmail.com>
2025-12-01 22:07:52 +0100

    #34 Add i18n

---
+++ userInterface/src/main/ts/src/i18n.ts
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-named-as-default-member */
+import i18n from "i18next";
+import HttpBackend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
+
+i18n
+	.use(initReactI18next)
+	.use(HttpBackend)
+	.init({
+		backend: {
+			loadPath: "/../public/locales/{{lng}}/translation.json",
+		},
+		lng: "en",
+		fallbackLng: "en",
+		debug: true,
+		supportedLngs: ["en", "pl"],
+		interpolation: {
+			escapeValue: false,
+		},
+	});
+
+export default i18n;
