8f206d78e062ccfde3a1668afa24c702da7f4fe1
mk1808 <37344587+mk1808@users.noreply.github.com>
2025-12-13 18:51:39 +0100

    #31 add env variables

--- userInterface/src/main/ts/vite.config.ts
+++ userInterface/src/main/ts/vite.config.ts
@@ -1,8 +1,25 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const API_URL = `${env.VITE_API ?? 'http://localhost:3000'}`;
+  // const PORT = `${env.VITE_PORT ?? '3000'}`;
+
+  return {
+    plugins: [react(), tsconfigPaths()],
+    server: {
+      proxy: {
+        '/api': {
+          target: API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  };
 });
+
