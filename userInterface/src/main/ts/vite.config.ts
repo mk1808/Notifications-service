14012d04a30faa87deeead2f3f66ac7985c0f905
mk1808 <37344587+mk1808@users.noreply.github.com>
2025-12-13 19:34:44 +0100

    #31 add url mappings

--- userInterface/src/main/ts/vite.config.ts
+++ userInterface/src/main/ts/vite.config.ts
@@ -2,21 +2,25 @@ import react from "@vitejs/plugin-react";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
-
-  const API_URL = `${env.VITE_API ?? 'http://localhost:3000'}`;
-  // const PORT = `${env.VITE_PORT ?? '3000'}`;
+  const API_URL = `${env.VITE_API ?? 'http://localhost:8072'}`;
+  const NOTIFICATIONS_URL = `${env.VITE_API_NOTIFICATIONS ?? '/NOTIFICATIONS/api/notifications'}`;
+  const CONFIG_URL = `${env.VITE_API_CONFIG ?? '/NOTIFICATIONS/api/config'}`;
 
   return {
     plugins: [react(), tsconfigPaths()],
     server: {
       proxy: {
-        '/api': {
+        '/notifications': {
           target: API_URL,
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
+          rewrite: (path) => path.replace(/^\/notifications/, NOTIFICATIONS_URL)
+        },
+        '/config': {
+          target: API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/config/, CONFIG_URL)
         },
       },
     },
