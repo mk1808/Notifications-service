085c9793e91e0b0d4114d9440226984effade873
mk1808 <37344587+mk1808@users.noreply.github.com>
2025-12-13 21:52:22 +0100

    #31 fixes after pr

--- userInterface/src/main/ts/vite.config.ts
+++ userInterface/src/main/ts/vite.config.ts
@@ -4,10 +4,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
-  const API_URL = `${env.VITE_API ?? 'http://localhost:8072'}`;
+  const API_URL = env.VITE_API;
 
-  const NOTIFICATIONS_URL = `${env.VITE_API_NOTIFICATIONS_PROXY ?? '/NOTIFICATIONS/api/notifications'}`;
-  const CONFIG_URL = `${env.VITE_API_CONFIG_PROXY ?? '/NOTIFICATIONS/api/config'}`;
+  const NOTIFICATIONS_URL = env.VITE_API_NOTIFICATIONS_PROXY;
+  const CONFIG_URL = env.VITE_API_CONFIG_PROXY;
 
   return {
     plugins: [react(), tsconfigPaths()],
