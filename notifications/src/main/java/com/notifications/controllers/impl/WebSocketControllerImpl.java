9c3c21c01ea997c9e73b859e02ffe239888ee7a7
mk1808 <37344587+mk1808@users.noreply.github.com>
2025-12-24 00:04:10 +0100

    #55 add backend for websocket connection and message sending

--- notifications/src/main/java/com/notifications/controllers/impl/WebSocketControllerImpl.java
+++ notifications/src/main/java/com/notifications/controllers/impl/WebSocketControllerImpl.java
@@ -1,7 +1,33 @@
 package com.notifications.controllers.impl;
 
-import com.notifications.controllers.WebSocketController;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
 
+import com.notifications.controllers.WebSocketController;
+import com.notifications.dtos.NotificationDto;
+
+@Controller
 public class WebSocketControllerImpl implements WebSocketController {
+
+	@MessageMapping("/chat.sendMessage")
+	@SendTo("/topic/public")
+	public NotificationDto sendMessage(@Payload NotificationDto chatMessage) {
+		System.out.println("sendMessage");
+		System.out.println(chatMessage.content);
+		return chatMessage;
+	}
+
+	@MessageMapping("/chat.addUser")
+	@SendTo("/topic/public")
+	public NotificationDto addUser(@Payload NotificationDto chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		System.out.println("addUser");
+		System.out.println(chatMessage.content);
+		headerAccessor.getSessionAttributes().put("username", chatMessage.recipient);
+
+		return chatMessage;
+	}
 
 }
--- notifications/src/main/java/com/notifications/workers/WebSocketEventListener.java
+++ notifications/src/main/java/com/notifications/workers/WebSocketEventListener.java
@@ -1,5 +1,33 @@
 package com.notifications.workers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
 public class WebSocketEventListener {
+	private final SimpMessageSendingOperations messagingTemplate;
+
+	Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+
+	public WebSocketEventListener(SimpMessageSendingOperations messagingTemplate) {
+		super();
+		this.messagingTemplate = messagingTemplate;
+	}
+
+	@EventListener
+	public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+		String username = (String) headerAccessor.getSessionAttributes().get("username");
+
+		if (username != null) {
+			logger.info("User Disconnected : " + username);
+
+		}
+	}
 
 }
