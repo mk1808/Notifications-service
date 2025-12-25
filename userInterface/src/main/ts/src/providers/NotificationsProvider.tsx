5db5d683081ae5f7ede2167c83182aa2ce733cd8
mk1808 <37344587+mk1808@users.noreply.github.com>
2025-12-25 21:23:12 +0100

    #26 add basic notifications provider

---
+++ userInterface/src/main/ts/src/providers/NotificationsProvider.tsx
@@ -0,0 +1,88 @@
+import {
+	createContext,
+	useCallback,
+	useMemo,
+	useReducer,
+	type ReactNode,
+} from "react";
+
+import type { Action, Api } from "@/types/types";
+
+export const NotificationContext = createContext({});
+export const NotificationActionContext = createContext({});
+
+export const ADD = "ADD";
+export const ACKNOWLEDGE = "ACKNOWLEDGE";
+export const DELETE = "DELETE";
+
+interface NotificationsState {
+	list: Api.NotificationDto[];
+	numberOfnew: number;
+}
+
+const initialState: NotificationsState = {
+	list: [],
+	numberOfnew: 0,
+};
+
+function reducer(state: NotificationsState, action: Action) {
+	switch (action.type) {
+		case ADD: {
+			console.log("add");
+			return {
+				...state,
+			};
+		}
+		case ACKNOWLEDGE: {
+			console.log("acknowledge");
+			return {
+				...state,
+			};
+		}
+		case DELETE: {
+			console.log("delete");
+			return {
+				...state,
+			};
+		}
+		default:
+			return state;
+	}
+}
+
+const NotificationsProvider = ({ children }: { children: ReactNode }) => {
+	const [state, dispatch] = useReducer<NotificationsState, [action: Action]>(
+		reducer,
+		initialState,
+	);
+
+	const add = useCallback((notification: Api.NotificationDto) => {
+		dispatch({ type: ADD, data: { notification } });
+	}, []);
+
+	const acknowledge = useCallback((notification: Api.NotificationDto) => {
+		dispatch({ type: ACKNOWLEDGE, data: { notification } });
+	}, []);
+
+	const deleteN = useCallback((notification: Api.NotificationDto) => {
+		dispatch({ type: DELETE, data: { notification } });
+	}, []);
+
+	const notificationsValue = useMemo(() => state, [state]);
+	const notificationsActionValue = useMemo(
+		() => ({
+			add,
+			acknowledge,
+			deleteN,
+		}),
+		[add, acknowledge, deleteN],
+	);
+	return (
+		<NotificationContext value={notificationsValue}>
+			<NotificationActionContext value={notificationsActionValue}>
+				{children}
+			</NotificationActionContext>
+		</NotificationContext>
+	);
+};
+export default NotificationsProvider;
